7eaa0a8b3286423da927cd8ae6b914b91b6f32f6
Dina Rosvoll <dinaro@stud.ntnu.no>
2019-05-16 10:51:16 +0200

    jhh

--- tensorkeras.py
+++ tensorkeras.py
@@ -44,7 +44,7 @@ model.add(Embedding(input_dim=num_words,
                     name='layer_embedding'))
 
 # Layers
-model.add(LSTM(units=12, dropout=0.1, recurrent_dropout=0.1))
+model.add(LSTM(units=24, dropout=0.3, recurrent_dropout=0.2))
 model.add(Dense(3, activation='softmax'))
 
 tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
